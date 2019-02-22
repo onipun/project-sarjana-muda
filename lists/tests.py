125f637db25edd8a2a7c8254abca465236f2b4fb
soneca <sonectool@gmail.com>
2019-02-22 16:24:22 +0800

    revert

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from lists.views import home_page 
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.models import item
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
@@ -54,3 +54,15 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+    def test_can_save_a_POST_request(self):    
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')  
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')        
+        self.assertEqual(Item.objects.count(), 0)
--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from lists.models import Item
 
 # Create your views here.
 def home_page(request):
     # if request.method == 'POST': 
     #    return HttpResponse(request.POST['item_text'])
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+    else:        
+        new_item_text = ''
 
     return render(request, 'home.html', {
         # get -> returing 'item_text' if no, return default which is second argm
-        'new_item_text': request.POST.get('item_text',''),
+        'new_item_text': new_item_text,
     })
